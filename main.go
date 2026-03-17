b28bb3874158e265b75a993cb5c5e04052a11a71
Coco <heyanyaray@gmail.com>
2026-03-16 18:20:24 -0700

    fixed API uploading to imgchest, now functions at basic level

--- main.go
+++ main.go
@@ -12,9 +12,14 @@ import (
 )
 
 func main() {
-	restTest := restGet("https://api.restful-api.dev/objects")
-	log.Println(restTest)
+	//restTest := restGet("https://api.restful-api.dev/objects")
+	//log.Println(restTest)
 	runBot()
+	/*link, err := restPutImg("https://media.discordapp.net/attachments/541836238471299114/1473448726696558743/HArtXgCbYAAtan7.png?ex=69963f8f&is=6994ee0f&hm=b8edb7d0ba5408ad5f3e3e52ab622b435546f79995e6f6b536ee69e2ba119fe4&")
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Println("Successfully uploaded img at: " + link)*/
 }
 
 func runBot() {
@@ -51,10 +56,11 @@ func helloMesages(s *discordgo.Session, m *discordgo.MessageCreate) {
 		//check if there's any attatchments and loop over them
 		if len(m.Attachments) > 0 {
 			for _, attatchment := range m.Attachments {
-				//for any image attatchemnts, log and print out the image proxy url.
+				//for any image attatchements, log and print out the image proxy url.
 				if strings.HasPrefix(attatchment.ContentType, "image") {
 					log.Printf("image found: %+v\n", attatchment.ProxyURL)
 					s.ChannelMessageSend(m.ChannelID, "Oh boy yummy image for me! "+attatchment.ProxyURL)
+					restPutImg(attatchment.ProxyURL)
 				} else {
 					//for non-image attatchments
 					log.Println(attatchment.ContentType)
@@ -66,31 +72,35 @@ func helloMesages(s *discordgo.Session, m *discordgo.MessageCreate) {
 			s.ChannelMessageSend(m.ChannelID, "Hey that's me! Gimme an image I've been so good :D")
 		}
 	} else if strings.Contains(strings.ToLower(m.Content), "good boy") {
-		//Good Boy GIFs, replies with any of the following
-		goodBoyGIF := ""
-		switch rand.IntN(8) {
-		case 1:
-			goodBoyGIF = "https://c.tenor.com/_4xCiEhhoZsAAAAd/tenor.gif"
-		case 2:
-			goodBoyGIF = "https://c.tenor.com/4jSFH4ktsHsAAAAC/tenor.gif"
-		case 3:
-			goodBoyGIF = "https://c.tenor.com/vXG7hQc33IoAAAAC/tenor.gif"
-		case 4:
-			goodBoyGIF = "https://c.tenor.com/cDJj3LEw0UIAAAAd/tenor.gif"
-		case 5:
-			goodBoyGIF = "https://c.tenor.com/ts6NhvDRU1AAAAAd/tenor.gif"
-		case 6:
-			goodBoyGIF = "https://c.tenor.com/qIxZz0K4yVQAAAAC/tenor.gif"
-		case 7:
-			goodBoyGIF = "https://tenor.com/view/straight-face-inuyasha-inuyahsa-gif-21752958"
-		default:
-			goodBoyGIF = "https://c.tenor.com/KY0JkwS42xEAAAAd/tenor.gif"
 
-		}
-
-		s.ChannelMessageSend(m.ChannelID, goodBoyGIF)
+		s.ChannelMessageSend(m.ChannelID, getAGoodBoy())
 
 	}
+}
+
+func getAGoodBoy() string {
+	//Good Boy GIFs, replies with any of the following
+	goodBoyGIF := ""
+	switch rand.IntN(8) {
+	case 1:
+		goodBoyGIF = "https://c.tenor.com/_4xCiEhhoZsAAAAd/tenor.gif"
+	case 2:
+		goodBoyGIF = "https://c.tenor.com/4jSFH4ktsHsAAAAC/tenor.gif"
+	case 3:
+		goodBoyGIF = "https://c.tenor.com/vXG7hQc33IoAAAAC/tenor.gif"
+	case 4:
+		goodBoyGIF = "https://c.tenor.com/cDJj3LEw0UIAAAAd/tenor.gif"
+	case 5:
+		goodBoyGIF = "https://c.tenor.com/ts6NhvDRU1AAAAAd/tenor.gif"
+	case 6:
+		goodBoyGIF = "https://c.tenor.com/qIxZz0K4yVQAAAAC/tenor.gif"
+	case 7:
+		goodBoyGIF = "https://tenor.com/view/straight-face-inuyasha-inuyahsa-gif-21752958"
+	default:
+		goodBoyGIF = "https://c.tenor.com/KY0JkwS42xEAAAAd/tenor.gif"
+
+	}
+	return goodBoyGIF
 }
 
 func loadFile(filename string) string {
--- restUtil.go
+++ restUtil.go
@@ -2,15 +2,18 @@ package main
 
 import (
 	"bytes"
-	"encoding/base64"
 	"encoding/json"
+	"errors"
 	"fmt"
 	"io"
 	"log"
+	"mime/multipart"
 	"net/http"
+	"strconv"
 	"time"
 )
 
+// throws a basic GET request at the provided endpoint
 func restGet(url string) string {
 	resp, err := http.Get(url)
 	errorCheck(err)
@@ -26,82 +29,144 @@ func restGet(url string) string {
 	return string(data)
 }
 
-func restPutImg(imgUrl string) {
-	// Replace with the API ID provided by imgchest
-	apiID := "XXXXXX"
+// uploads a single image to imgchest, returns a link to the image.
+func restPutImg(imgUrl string) (string, error) {
+
+	if len(imgUrl) == 0 {
+		return "", errors.New("Blank URL Provided to REST PUT")
+	}
+
+	// loads API key from a file called imgchest.key
+	apiKey := loadFile("imgchest.key")
+	if len(apiKey) == 0 {
+		return "", errors.New("imgchest.key file empty or does not exist, please create file and populate with an ImgChest API key")
+	}
 
 	// HTTP endpoint for uploading new posts
-	createResourceURL := "https://api.imgchest.com/v1/post"
+	createPostEndpoint := "https://api.imgchest.com/v1/post"
 
-	// Declare these here so they can be used in the below two if blocks and
-	// still carry over to the end of this function.
-	var base64img string
-
-	// pull image from URL and convert into a base64 encoding of bytes to send to imgchest
-	if imgUrl != "" {
-
-		resp, err := http.Get(imgUrl)
-		if err != nil {
-			fmt.Println("Error retrieving the file, ", err)
-			return
-		}
-
-		defer func() {
-			_ = resp.Body.Close()
-		}()
-
-		img, err := io.ReadAll(resp.Body)
-		if err != nil {
-			fmt.Println("Error reading the response, ", err)
-			return
-		}
-
-		//contentType = http.DetectContentType(img)
-		base64img = base64.StdEncoding.EncodeToString(img)
-	}
-
-	//title string based on date and random number
-	titleString := "Chester Upload " + time.Now().Format("RFC3339Nano")
-
-	// JSON payload containing resource information
-	requestBody, _ := json.Marshal(map[string]string{
-		"title":     titleString,
-		"privacy":   "secret",
-		"anonymous": "false",
-		"nsfw":      "false",
-		"images":    "[" + base64img + "]",
-	})
-
-	// Converting request body into bytes buffer
-	requestBodyBytes := bytes.NewBuffer(requestBody)
-
-	// Creating a POST request on createResourceURL
-	request, err := http.NewRequest("POST", createResourceURL, requestBodyBytes)
+	// pull image from URL
+	resp, err := http.Get(imgUrl)
 	if err != nil {
-		panic(err)
+		return "", errors.Join(errors.New("Error retrieving the file, "), err)
 	}
 
-	// Adding application/json as payload type
-	request.Header.Add("Content-Type", "application/json")
+	defer resp.Body.Close()
+
+	//error out if we don't get a good response code
+	if resp.StatusCode != 200 {
+		return "", errors.New("Recieved response code " + strconv.Itoa(resp.StatusCode) + " " + resp.Status + " when trying to retrieve image from url " + imgUrl)
+	}
+	//read the image to memory
+	img, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return "", errors.Join(errors.New("Error reading the response body, "), err)
+	}
+
+	//title string based on date and current time
+	titleString := "Chester Upload " + time.Now().Format(time.RFC3339Nano)
+
+	/*Now, to upload to the ImgChest API.
+	  Documentation for this is extremely vague about how this works, but, it requires a multipart form data request
+	  using mime/multipart, we set up a form, write each field (per API documentation) and then write the image.
+	  Then we create a POST request, set this form as the body, add headers, and send it.
+
+	  The images[] field is the trickiest part and took me a lot of looking around to understand.
+	  in this case, create a Form File, name it images[], and then pass it the names of the image being uploaded.
+	  Have yet to test it with more than one image at a time but hey this works great for now.
+
+	  TODO: figure out how to set the Anonymous field to exactly a boolean value as the API expects.
+	  Writing it as is throws an error that the
+	*/
+
+	bodyBuffer := &bytes.Buffer{}
+	mpw := multipart.NewWriter(bodyBuffer)
+	mpw.WriteField("title", titleString)
+	mpw.WriteField("privacy", "secret")
+	//mpw.WriteField("anonymous", "false")
+	imgWriter, err := mpw.CreateFormFile("images[]", "webp.webp")
+	if err != nil {
+		return "", errors.Join(errors.New("Error constructing Form Data: "), err)
+	}
+	imgWriter.Write(img)
+	mpw.Close()
+
+	// Creating a POST request on createPostEndpoint
+	request, err := http.NewRequest("POST", createPostEndpoint, bodyBuffer)
+	if err != nil {
+		return "", err
+	}
+
+	// Setting Headers
+	//Content type to tell the API this is a multipart form, and we accept a JSON response
+	request.Header.Add("Content-Type", mpw.FormDataContentType())
+	request.Header.Add("Accept", "application/json")
 	//Adding API Key as a header
-	request.Header.Add("Authorization", apiID)
+	request.Header.Add("Authorization", "Bearer "+apiKey)
 
 	client := &http.Client{}
 
 	// Performing POST request
 	response, err := client.Do(request)
 	if err != nil {
-		panic(err)
+		return "", err
 	}
-
 	defer response.Body.Close()
 
 	// Reading the response to the request
 	responseBody, err := io.ReadAll(response.Body)
 	if err != nil {
-		fmt.Println("Error received in response:", err)
+		return "", err
 	}
 
+	var responseStruct postResponse
+	err = json.Unmarshal(responseBody, &responseStruct)
+	if err != nil || len(responseStruct.Data.Images) == 0 {
+		return "", errors.Join(errors.New("error in API response: "+string(responseBody)+" - "), err)
+	}
+	//debug
 	responseBodyString := string(responseBody)
 	fmt.Println(responseBodyString)
+
+	//return a link to the created image on imgChest
+	return responseStruct.Data.Images[0].Link, nil
+}
+func deserializeTest() (string, error) {
+	input := []byte(`{"data":{"id":"ne7bnmvzj75","title":"Chester Upload 2026-02-17T19:25:04.340400342-08:00","username":"cocoflanne728","privacy":"secret","report_status":1,"views":0,"nsfw":0,"image_count":1,"created":"2026-02-18T03:25:04.000000Z","delete_url":"https:\/\/imgchest.com\/p\/7pyveh6m9jk48\/delete","images":[{"id":"162e2f602561","description":null,"link":"https:\/\/cdn.imgchest.com\/files\/162e2f602561.webp","position":1,"nsfw":false,"created":"2026-02-18T03:25:04.000000Z","original_name":"webp.webp"}]}}`)
+	var responseStruct postResponse
+	err := json.Unmarshal(input, &responseStruct)
+	if err != nil || len(responseStruct.Data.Images) == 0 {
+		return "", errors.Join(errors.New("error in API response: "+string(input)+" - "), err)
+	}
+	//debug
+	responseBodyString := string(input)
+	fmt.Println(responseBodyString)
+
+	//return a link to the created image on imgChest
+	return responseStruct.Data.Images[0].Link, nil
+}
+
+type imgFileInfo struct {
+	Id            string `json:"id"`
+	Description   string `json:"description"`
+	Link          string `json:"link"`
+	Position      int    `json:"position"`
+	Nsfw          bool   `json:"nsfw"`
+	Created       string `json:"created"`
+	Original_name string `json:"original_name"`
+}
+type postResponse struct {
+	Data struct {
+		Id            string        `json:"id"`
+		Title         string        `json:"title"`
+		Username      string        `json:"username"`
+		Privacy       string        `json:"privacy"`
+		Report_status int           `json:"report_status"`
+		Views         int           `json:"views"`
+		Nsfw          int           `json:"nsfw"`
+		Image_count   int           `json:"image_count"`
+		Created       string        `json:"created"`
+		Delete_url    string        `json:"delete_url"`
+		Images        []imgFileInfo `json:"images"`
+	} `json:"data"`
 }
